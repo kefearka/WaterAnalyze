b910de967ab7abd677fd0d14eba0e89ba3bf6712
kefearka <tonybalkony@gmail.com>
2024-04-08 16:47:46 +0500

    event constructor

--- ClassApp.py
+++ ClassApp.py
@@ -7,9 +7,12 @@ class Application:
     main_win = 0
 
     def __init__(self):
+        self.__make_events()
+        self.main_win = MainWindow()
+
+    def __make_events(self):
         xevents.add_event("Application run", self.exec, 0)
         xevents.add_event("Application quit", self.on_quit, 0)
-        self.main_win = MainWindow()
 
     def exec(self):
         self.main_win.run()
