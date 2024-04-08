a4bcb4ed90d30cd796e9b43bd6439543e8889d1e
kefearka <tonybalkony@gmail.com>
2024-04-08 16:37:59 +0500

    end

--- ClassApp.py
+++ ClassApp.py
@@ -9,6 +9,7 @@ class Application:
     def __init__(self):
         xevents.add_event("Application run", self.exec, 0)
         xevents.add_event("Application quit", self.on_quit, 0)
+        print()
         self.main_win = MainWindow()
 
     def exec(self):
